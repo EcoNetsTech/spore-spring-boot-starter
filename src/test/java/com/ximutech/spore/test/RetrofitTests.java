756a0ed366195ceacc399b8fe393c8aa1a3b580c
ryximu233 <ryximu233@foxmail.com>
2023-11-22 02:05:03 +0800

    update

--- src/test/java/com/ximutech/spore/test/RetrofitTests.java
+++ src/test/java/com/ximutech/spore/test/RetrofitTests.java
@@ -2,9 +2,7 @@ package com.ximutech.spore.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
